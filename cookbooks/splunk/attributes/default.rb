61a1af651929859cd7898b45fee859e6518eb053
msausa <mnikki137@gmail.com>
2013-01-29 22:10:42 +0000

    added attributes to the splunk cookbook

--- cookbooks/splunk/attributes/default.rb
+++ cookbooks/splunk/attributes/default.rb
@@ -1,5 +1,4 @@
-
-set [:splunk][:installdir] = "/opt/splunk"
-default [:splunk][:user] = "splunk"
-default [:splunk][:installfile] ="https://rs-training-assets.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
+set[:splunk][:installdir] = "/opt/splunk"
+default[:splunk][:user] = "splunk"
+default[:splunk][:installfile] ="https://rs-training-assets.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
 
